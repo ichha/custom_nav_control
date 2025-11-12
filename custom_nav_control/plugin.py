b9dc2a2470d54bed3333f366ef11df1601bb5856
ichha <thapa.ichha@gmail.com>
2025-11-12 07:24:42 +0545

    Update plugin.py

--- custom_nav_control/plugin.py
+++ custom_nav_control/plugin.py
@@ -1,32 +1,11 @@
-from netbox.plugins import PluginConfig
-from django.contrib.auth.signals import user_logged_in
-from netbox.registry import registry
+from extras.plugins import PluginConfig
 
-
-def hide_ipam_menu(user):
-    """
-    Hide the IPAM menu for non-superusers.
-    """
-    if not user.is_superuser:
-        if 'IPAM' in registry['menus']:
-            registry['menus'].pop('IPAM', None)
-
-
-def handle_user_login(sender, user, request, **kwargs):
-    """
-    Run when a user logs in to remove menus dynamically.
-    """
-    hide_ipam_menu(user)
-
-
-# Connect signal
-user_logged_in.connect(handle_user_login)
-
-
-class CustomNavControlConfig(PluginConfig):
-    name = 'custom_nav_control'
-    verbose_name = 'Custom Navigation Control'
-    description = 'Hide IPAM menu for non-superusers'
-    version = '1.0'
+class ExternalLinksSuperuserConfig(PluginConfig):
+    name = 'external_links_superuser'
+    verbose_name = 'External Links for Superuser'
+    description = 'Adds a custom left sidebar menu with external links visible only to superusers'
+    version = '1.0.0'
     author = 'Your Name'
-    base_url = 'custom-nav-control'
+    author_email = 'you@example.com'
+
+config = ExternalLinksSuperuserConfig
